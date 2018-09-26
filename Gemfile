a5a03851368b1cf5a2b3c49769443f47a5ecd7fa
Dean Nasseri <dnasseri10@gmail.com>
2018-09-26 12:21:19 -0400

    drop support for mysql

--- Gemfile
+++ Gemfile
@@ -5,6 +5,5 @@ gemspec
 gem "codeclimate-test-reporter", require: false
 gem "database_cleaner"
 gem "factory_girl_rails"
-gem "mysql2"
 gem "pg"
 gem "rspec-rails", "~> 3.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ Bundler.require
 plugin_test_dir = File.dirname(__FILE__)
 ActiveRecord::Base.logger = Logger.new(plugin_test_dir + "/debug.log")
 ActiveRecord::Base.configurations = YAML::load(IO.read(plugin_test_dir + "/db/database.yml"))
-ActiveRecord::Base.establish_connection((ENV["DB"] || "mysql").to_sym)
+ActiveRecord::Base.establish_connection(:postgresql)
 ActiveRecord::Migration.verbose = false
 load(File.join(plugin_test_dir, "db", "schema.rb"))
 
